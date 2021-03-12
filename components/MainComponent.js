973999070aaa206adc7643947f7d7a4c073f3445
Ujjwal <meujjwal21@gmail.com>
2021-03-12 15:24:34 +0530

    Image picker

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -138,7 +138,7 @@ const FavoritesNavigator = createStackNavigator({
 });
 
 const LoginNavigator = createStackNavigator({
-  Login: { screen: Login }
+  Login: Login
 }, {
 navigationOptions: ({ navigation }) => ({
   headerStyle: {
